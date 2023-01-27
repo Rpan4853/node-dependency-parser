13b4b90e46e1b705c5a7e82526bc6b500d275f2a
rpan4853 <raymondpan12345@gmail.com>
2023-01-27 14:12:27 -0500

    Implemented script

---
+++ src/helpers.ts
@@ -0,0 +1,76 @@
+import { createReadStream } from "fs";
+import { DiffLine } from "nodegit";
+import { createInterface } from "readline";
+import { DependencyMapping } from ".";
+
+export async function getJsonFieldBounds(
+  filePath: string,
+  fieldName: string,
+): Promise<{ start: number; end: number }> {
+  const fileStream = createReadStream(filePath);
+
+  const rl = createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+  let lineNum = 0;
+  let start = 0;
+  let insideField = false;
+  for await (const line of rl) {
+    // Each line in input.txt will be successively available here as `line`.
+    lineNum += 1;
+    if (line.includes(fieldName)) {
+      start = lineNum;
+      insideField = true;
+    }
+    if (insideField && line.includes("}")) {
+      return { start, end: lineNum };
+    }
+  }
+  throw `fieldName ${fieldName} not found in ${filePath}`;
+}
+
+export function updatePatchChanges(
+  patchChanges: DependencyMapping,
+  line: DiffLine,
+): void {
+  const parsedLine = line.content().trim().split(" ");
+  const [name, version] = parsedLine.map((val) =>
+    val.at(-1) == "," ? val.slice(0, -1) : val,
+  );
+  //line was removed
+  if (line.origin() == 45) {
+    if (name in patchChanges && patchChanges[name].type == "added") {
+      if (patchChanges[name].version == version) {
+        delete patchChanges[name];
+      } else {
+        patchChanges[name].type = "updated";
+        patchChanges[name].prevVersion = version;
+      }
+    } else if (!(name in patchChanges)) {
+      patchChanges[name] = {
+        name,
+        version,
+        type: "removed",
+      };
+    }
+  }
+  //line was added
+  else if (line.origin() == 43) {
+    if (name in patchChanges && patchChanges[name].type == "removed") {
+      if (patchChanges[name].version == version) {
+        delete patchChanges[name];
+      } else {
+        patchChanges[name].type = "updated";
+        patchChanges[name].prevVersion = patchChanges[name].version;
+        patchChanges[name].version = version;
+      }
+    } else if (!(name in patchChanges)) {
+      patchChanges[name] = {
+        name,
+        version,
+        type: "added",
+      };
+    }
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,92 @@
+import { Repository } from "nodegit";
+import path from "path";
+import { getJsonFieldBounds, updatePatchChanges } from "./helpers";
+//Removed dependencies, added dependencies, updated dependencies (versions)
+
+export interface Dependency {
+  name: string;
+  version: string;
+  type: "removed" | "added" | "updated";
+  prevVersion?: string;
+}
+
+export interface DependencyMapping {
+  [name: string]: Dependency;
+}
+
+export interface DepdendencyDiff {
+  removed: Dependency[];
+  added: Dependency[];
+  updated: Dependency[];
+}
+
+export interface FileDiff {
+  dependencies: DepdendencyDiff[];
+  devDependencies: DepdendencyDiff[];
+}
+export interface CommitDiffMapping {
+  [fileName: string]: FileDiff;
+}
+
+async function parseDependeciesDiff() {
+  //map file path to added, deleted, and/or updateed dependencies
+  const commitDiffMapping: CommitDiffMapping = {};
+
+  const repo = await Repository.open(path.resolve(__dirname, "../.git"));
+  const currentBranchReference = await repo.getCurrentBranch();
+  const latestCommit = await repo.getBranchCommit(currentBranchReference);
+  const diffList = await latestCommit.getDiff();
+  for (const diff of diffList) {
+    const patches = await diff.patches();
+
+    for (const patch of patches) {
+      const fileName = patch.newFile().path().split("/").at(-1);
+      if (fileName == "package.json") {
+        const patchChanges: DependencyMapping = {};
+        const patchDevChanges: DependencyMapping = {};
+
+        //get line ranges for depencies and devDependencies
+        const dependenciesBounds = await getJsonFieldBounds(
+          patch.newFile().path(),
+          `"dependencies`,
+        );
+        const devDependenciesBounds = await getJsonFieldBounds(
+          patch.newFile().path(),
+          `"devDependencies"`,
+        );
+
+        const hunks = await patch.hunks();
+        for (const hunk of hunks) {
+          const lines = await hunk.lines();
+          let lineCount = hunk.newStart() - 1;
+
+          for (const line of lines) {
+            //line.origin() returns an ASCII charCode, 43 is a +, 45 is a -
+            if (line.origin() != 45) {
+              lineCount++;
+            }
+
+            //lines are within dependencies or devDepencies range
+            if (
+              dependenciesBounds.start <= lineCount &&
+              dependenciesBounds.end >= lineCount &&
+              !line.content().trim().includes(`"dependencies`) &&
+              !line.content().trim().includes("}")
+            ) {
+              updatePatchChanges(patchChanges, line);
+            } else if (
+              devDependenciesBounds.start <= lineCount &&
+              devDependenciesBounds.end >= lineCount &&
+              !line.content().trim().includes(`"devDependencies`) &&
+              !line.content().trim().includes("}")
+            ) {
+              updatePatchChanges(patchDevChanges, line);
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+parseDependeciesDiff();
